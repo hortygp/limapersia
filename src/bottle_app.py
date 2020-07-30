ae71a674d25c2ee7890903c04e393a7c15ebf2e3
Hortygp <32112044+hortygp@users.noreply.github.com>
2020-07-29 22:29:36 -0300

    adiciona uma nova rota

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -1,10 +1,26 @@
 # -*- coding: UTF-8 -*-
-# A very simple Bottle Hello World app for you to get started with...
+
+"""Tutoria dois - brincando de git
+..codeautor:: Autor: Isabel Hortencia <hortencia.garnica@nce.ufrj.br>
+. Como criar um repositorio no github
+. como clonar usando o comando git
+. como commitar usando o comando git
+
+sem classes neste modulo:
+   
+Changelog
+---------
+..versionadded::    20.07
+       Adiciona o gerenciador de chamadas http via bottle
+"""
 from bottle import default_app, route
 
 @route('/')
 def hello_world():
     return 'Tutorial 2 aprendendo Git e Bottle!'
 
+@route('/oi')
+def oi_mundo():
+    return 'Tutorial 2 ensaiando uma nova rota'
 application = default_app()
 
