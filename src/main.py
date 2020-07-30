19121db8a061a5fadfd748b3230099887da60887
hortygp <hortygp@gmail.com>
2020-07-30 00:23:19 +0000

    adiciona o modulo main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+#limapercia.main.py
+"""Tutoria dois - brincando de git
+
+..codeautor:: Autor: Isabel Hortencia <hortencia.garnica@nce.ufrj.br>
+
+. Como criar um repositorio no github
+. como clonar usando o comando git
+. como commitar usando o comando git
+
+Classes neste modulo:
+
+    :py:class: 'Main' exemplos de classe qualquer.
+
+Changelog
+---------
+..versionadded::    20.07
+       Documentacao do tutorial.
+"""
+
+class Main:
+    """classe exemplo.
+
+         :param versao: versao deste exemplo.
+
+    """
+    def __init__(self, versao):
+        print("classe exemplo, versao{}".format(versao))
+
+if __name__ == "__main__":
+    Main(1)
